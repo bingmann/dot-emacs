75dff220f1c6477846676f42279746b4e3931b2a
Timo Bingmann <tbgit@panthema.net>
2017-03-04 08:19:12 +0100

    copied a CamelCase to snake_case converter

--- init.el
+++ init.el
@@ -1230,6 +1230,34 @@
 (define-key comint-mode-map (kbd "<up>") 'comint-previous-input)
 (define-key comint-mode-map (kbd "<down>") 'comint-next-input)
 
+;; ----------------------------------------
+;; --- CamelCase to snake_case and back ---
+;; ----------------------------------------
+
+(defun split-name (s)
+  (split-string
+   (let ((case-fold-search nil))
+     (downcase
+      (replace-regexp-in-string "\\([a-z]\\)\\([A-Z]\\)" "\\1 \\2" s)))
+   "[^A-Za-z0-9]+"))
+
+(defun camelcase  (s) (mapconcat 'capitalize (split-name s) ""))
+(defun underscore (s) (mapconcat 'downcase   (split-name s) "_"))
+
+(defun camelscore (s)
+  (cond ((string-match-p "-" s)         (colonize s))
+        ((string-match-p "_" s)	        (dasherize s))
+        (t                              (underscore s))))
+
+(defun camelscore-word-at-point ()
+  (interactive)
+  (let* ((case-fold-search nil)
+         (beg (and (skip-chars-backward "[:alnum:]_") (point)))
+         (end (and (skip-chars-forward  "[:alnum:]_") (point)))
+         (txt (buffer-substring beg end))
+         (cml (camelscore txt)) )
+    (if cml (progn (delete-region beg end) (insert cml))) ))
+
 ;; ------------------------------------------------
 ;; --- Increment and Decrement Numbers at Point ---
 ;; ------------------------------------------------
